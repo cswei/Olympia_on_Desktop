bb320ed6f7ab085f82f37f7e8037e5a43f586f72
Charles Wei <charles.wei@torchmobile.com.cn>
2011-04-25 12:04:12 +0800

    remove redundant directories

--- webkitsupport/olympia/WebKitTools/Scripts/webkitpy/common/config/__init__.py
+++
@@ -1,6 +0,0 @@
-# Required for Python to search this directory for module files
-
-import re
-
-codereview_server_host = "wkrietveld.appspot.com"
-codereview_server_url = "https://%s/" % codereview_server_host
--- webkitsupport/olympia/WebKitTools/Scripts/webkitpy/tool/commands/__init__.py
+++
@@ -1,4 +0,0 @@
-# Required for Python to search this directory for module files
-
-from webkitpy.tool.commands.prettydiff import PrettyDiff
-# FIXME: Add the rest of the commands here.
