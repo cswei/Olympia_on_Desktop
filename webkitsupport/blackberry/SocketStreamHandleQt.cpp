9b0c203bc419306a10df535deb9c9427458efc5e
Jonathan Dong <jonathan.dong@torchmobile.com.cn>
2011-05-04 10:56:52 +0800

    legal disclaimer added.

--- webkitsupport/blackberry/SocketStreamHandleQt.cpp
+++ webkitsupport/blackberry/SocketStreamHandleQt.cpp
@@ -1,6 +1,7 @@
 /*
  * Copyright (C) 2010 Nokia Inc. All rights reserved.
  * Copyright (C) 2009 Google Inc.  All rights reserved.
+ * Copyright (C) 2011 Torch Mobile (Beijing) Co. Ltd.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
